b1232380c280f111eed8112fdf21f9eb008b20b1
Jannis Maier <jannismaier97@gmail.com>
2023-05-09 16:44:32 +0200

    load dataset only once and plot all results

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # or any {'0', '1', '2'}
 
-from network import TestNetwork
+from network import TrainData, TestNet
 from data import read_config
 
 
@@ -32,28 +32,50 @@ def get_range(start: float, end: float, step: float) -> list:
         start += step
     return array
 
-def test_params(lrate: float, bsize: int, afun: str) -> tuple:
+
+def test_params(lrate: float, bsize: int, afun: str, train_data: TrainData) -> tuple:
     if not is_activation_function(afun):
         raise ValueError("Invalid activation function")
-    network = TestNetwork(lrate, bsize, afun)
-    network.load_data()
-    val_loss, val_acc = network.train()
+    network = TestNet(lrate, bsize, afun)
+    val_loss, val_acc = network.train(train_data)
     return val_loss, val_acc
+
 
 def pretty_print(parameter: str, results: list):
     print("Results for " + parameter + ":")
     for result in results:
-        print("\t" + parameter.capitalize() + ": " + str(result[0]) + ", loss: " + str(result[1]) + ", accuracy: " + str(result[2]))
+        print(
+            "\t"
+            + parameter.capitalize()
+            + ": "
+            + str(result[0])
+            + ", loss: "
+            + str(result[1])
+            + ", accuracy: "
+            + str(result[2])
+        )
+
 
 def plot_results(parameter: str, results: list):
-    plt.plot([result[0] for result in results], [result[1] for result in results], label="loss")
-    plt.plot([result[0] for result in results], [result[2] for result in results], label="accuracy")
+    plt.plot(
+        [result[0] for result in results],
+        [result[1] for result in results],
+        label="loss",
+    )
+    plt.plot(
+        [result[0] for result in results],
+        [result[2] for result in results],
+        label="accuracy",
+    )
     plt.xlabel(parameter)
     plt.legend()
+    plt.tight_layout()
     plt.show()
+
 
 def main():
     config = read_config("configs/default.json")
+    train_data = TrainData()
 
     learning_rate = config["learning_rate"]
     batch_size = config["batch_size"]
@@ -65,28 +87,31 @@ def main():
     def_lrate = rates[round(len(rates) / 2)]
     def_bsize = sizes[round(len(sizes) / 2)]
     def_afunc = activation_functions[0]
-    
-    results = []
+
+    lrate_results = []
     for lrate in rates:
         print("\n\nTesting learning rate: " + str(lrate))
-        result = test_params(lrate, def_bsize, def_afunc)
-        results.append((lrate, result[0], result[1]))
-    pretty_print("learning rate", results)
-    plot_results("learning rate", results)
+        result = test_params(lrate, def_bsize, def_afunc, train_data)
+        lrate_results.append((lrate, result[0], result[1]))
+    # pretty_print("learning rate", lrate_results)
 
-    results = []
+    bsize_results = []
     for bsize in sizes:
         print("\n\nTesting batch size: " + str(bsize))
-        result = test_params(def_lrate, bsize, def_afunc)
-        results.append((bsize, result[0], result[1]))
-    pretty_print("batch size", results)
+        result = test_params(def_lrate, bsize, def_afunc, train_data)
+        bsize_results.append((bsize, result[0], result[1]))
+    # pretty_print("batch size", bsize_results)
 
-    results = []
+    afunc_results = []
     for afunc in activation_functions:
         print("\n\nTesting activation function: " + afunc)
-        result = test_params(def_lrate, def_bsize, afunc)
-        results.append((afunc, result[0], result[1]))
-    pretty_print("activation function", results)
+        result = test_params(def_lrate, def_bsize, afunc, train_data)
+        afunc_results.append((afunc, result[0], result[1]))
+    # pretty_print("activation function", afunc_results)
+
+    plot_results("learning rate", lrate_results)
+    plot_results("batch size", bsize_results)
+    plot_results("activation function", afunc_results)
 
 
 if __name__ == "__main__":
